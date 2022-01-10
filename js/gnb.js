fffc0aa76764075c402bef96e7329703518d61bc
jonghyeon <cjddnjfql@naver.com>
2022-01-10 17:51:11 +0900

    gnb.js 수정

--- js/gnb.js
+++ js/gnb.js
@@ -1,1 +1,25 @@
-$(document).ready(function () {});
+$(document).ready(function() {
+
+});
+const gnbBt = document.querySelectorAll(".gnbBt > a");
+const wrapInner = document.querySelectorAll('.wrapInner');
+var i = 0;
+
+function styleAdd() {
+  gnbBt[i].classList.add("over");
+  wrapInner[i].style.display = "block";
+};
+
+function styleRemove() {
+  gnbBt[i].classList.remove("over");
+  wrapInner[i].style.display = "none";
+}
+
+  gnbBt[i].addEventListener("mouseenter", styleAdd);
+  gnbBt[i].addEventListener('mouseleave', styleRemove);
+
+  [].forEach.call(gnbBt, function(el) {
+    el.addEventListener('mouseenter', function() {
+      console.log(getElementIndex(gnbBt, el));
+    });
+  });
