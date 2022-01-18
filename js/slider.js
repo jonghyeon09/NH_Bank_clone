34f9db8817bf86fb3327cde189941eea35deca95
jonghyeon <cjddnjfql@naver.com>
2022-01-18 17:49:33 +0900

    pager 클릭 수정중

--- js/slider.js
+++ js/slider.js
@@ -9,17 +9,20 @@ $(document).ready(function () {
   let count = 0;
   let click_check = false;
   let item_i;
+  let interval;
+  let e = false;
 
   //다음버튼
   $(".sliderContent").css({ left: -slide_w });
   $(".Next").click(function () {
-    if (count < 3 && click_check == false) {
-      count++;
+    if (count <= 3 && click_check == false) {
+      if (count == 3) {
+        count = 0;
+      } else {
+        count++;
+      }
       sliderCounter(count);
       pagerColor(count);
-      if (count == 3) {
-        count = -1;
-      }
     }
     if (click_check == false) {
       click_check = true;
@@ -65,33 +68,43 @@ $(document).ready(function () {
     }
   });
 
+  function Prev() {
+    $(".sliderContent").stop().animate();
+  }
   // 페이저 클릭
   $(pager_item).click(function () {
     item_i = $(this).index();
-    pagerColor(item_i);
-    slider_ani(item_i);
+    let page_count = item_i - count;
+    clearInterval(interval);
+    if (page_count < 0) {
+      console.log("왼" + Math.abs(page_count));
+    } else {
+      console.log("우" + page_count);
+    }
+
+    //pagerColor(item_i);
+    // slider_ani(item_i);
   });
 
   // 자동재생
   $(".autoBt").click(function () {
-    if (click_check == false) {
-      click_check = true;
-      clearInterval(sliderInInterval());
+    clearInterval(interval);
+    if (e == false) {
+      e = true;
       $(".autoBt a").removeClass("stop");
       $(".autoBt a").addClass("start");
-    } else if (click_check == true) {
-      click_check = false;
-      // $(sliderInInterval).clearQueue();
+    } else if (e == true) {
+      e = false;
       sliderInInterval();
       $(".autoBt a").removeClass("start");
       $(".autoBt a").addClass("stop");
     }
-    console.log(click_check);
+    console.log(e);
   });
 
   sliderInInterval();
   function sliderInInterval() {
-    setInterval(function () {
+    interval = setInterval(function () {
       $(".Next").trigger("click");
     }, 3000);
   }
@@ -107,27 +120,27 @@ $(document).ready(function () {
 
   $(pager_item[0]).children("a").addClass("active");
 
-  function slider_ani(i) {
-    if (i == 0) {
-      $(".sliderContent")
-        .stop()
-        .animate({ left: -slide_w * i }, 300);
-      pagerColor(i);
-    } else if (i == 1) {
-      $(".sliderContent")
-        .stop()
-        .animate({ left: -slide_w * i }, 300);
-      pagerColor(i);
-    } else if (i == 2) {
-      $(".sliderContent")
-        .stop()
-        .animate({ left: -slide_w * i }, 300);
-      pagerColor(i);
-    } else if (i == 3) {
-      $(".sliderContent")
-        .stop()
-        .animate({ left: -slide_w * i }, 300);
-      pagerColor(i);
-    }
-  }
+  // function slider_ani(i) {
+  //   if (i == 0) {
+  //     $(".sliderContent")
+  //       .stop()
+  //       .animate({ left: -slide_w * i }, 300);
+  //     pagerColor(i);
+  //   } else if (i == 1) {
+  //     $(".sliderContent")
+  //       .stop()
+  //       .animate({ left: -slide_w * i }, 300);
+  //     pagerColor(i);
+  //   } else if (i == 2) {
+  //     $(".sliderContent")
+  //       .stop()
+  //       .animate({ left: -slide_w * i }, 300);
+  //     pagerColor(i);
+  //   } else if (i == 3) {
+  //     $(".sliderContent")
+  //       .stop()
+  //       .animate({ left: -slide_w * i }, 300);
+  //     pagerColor(i);
+  //   }
+  // }
 });
