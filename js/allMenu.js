0696c54d54e4772b1e1de4275b3cd518f190bfd1
jonghyeon <cjddnjfql@naver.com>
2022-01-21 17:44:47 +0900

    전체메뉴보기 작업중

---
+++ js/allMenu.js
@@ -0,0 +1,5 @@
+const hamBt = document.getElementsByClassName("gnbH");
+const menuBt = document.querySelectorAll(".gnbAll");
+const allMenu = document.getElementsByClassName("wrap_allMenu");
+const dark = document.getElementsByClassName("back");
+$(document).ready(function () {});
